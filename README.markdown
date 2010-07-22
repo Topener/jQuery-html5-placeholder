126fda32902712b63956ecf0309c1dda2f36d17b
Mike Taylor <runningtowardsnothing@gmail.com>
2010-07-22 00:13:01 -0400

    Bumping up to 1.0. Thanks to Jeffrey Barke for tip on getting IE6 to play

--- README.markdown
+++ README.markdown
@@ -1,13 +1,16 @@
-# HTML5 placeholder plugin, version 0.3
+# HTML5 placeholder plugin, version 1.0
 Copyright (c) 2010-The End of Time, Mike Taylor, http://miketaylr.com
 
 MIT Licensed: http://www.opensource.org/licenses/mit-license.php
 
-Enables cross-browser* html5 placeholder for inputs, by first testing for a native implementation before building one.
+Enables cross-browser html5 placeholder for inputs, by first testing for a native implementation before building one.
 
 ##USAGE: 
 `$('input[placeholder]').placeholder();`
 
+`$('input[placeholder]').placeholder({
+   color: '#bada55'
+});`
+
 `<input type="text" placeholder="username">`
 
-###*NOTE: Totally busted in ie6. Fork it and fix it if you care.
--- html5placeholder.jquery.js
+++ html5placeholder.jquery.js
@@ -1,11 +1,10 @@
-// HTML5 placeholder plugin version 0.3
+// HTML5 placeholder plugin version 1.0
 // Copyright (c) 2010-The End of Time, Mike Taylor, http://miketaylr.com
 // MIT Licensed: http://www.opensource.org/licenses/mit-license.php
 //
 // Enables cross-browser* html5 placeholder for inputs, by first testing
 // for a native implementation before building one.
 //
-// *NOTE: Totally busted in ie6. Fork it and fix it if you care.
 //
 // USAGE: 
 //$('input[placeholder]').placeholder();
@@ -19,31 +18,36 @@
     //cache the original 'left' value, for use by Opera later
     o_left = opts.placeholderCSS.left;
   
-    //sniffy sniff sniff
-    var isOpera = function() {
-      return $.browser.opera;
+    //sniffy sniff sniff -- just to give extra left padding for the older
+    //graphics for type=email and type=url
+    var isOldOpera = function() {
+      return $.browser.opera && $.browser.version < 10.5;
     };
   
     //first test for native placeholder support before continuing
     //feature detection inspired by ye olde jquery 1.4 hawtness, with paul irish
-    return ('placeholder' in document.createElement('input')) ? this : this.each(function() {
+    return ('placeholder' in document.createElement('input')) ? 
+      this : this.each(function() {
     
       //local vars
       var $this = $(this),
           inputVal = $.trim($this.val()),
           inputWidth = $this.width(),
           inputHeight = $this.height(),
-          //grab the inputs id for the <label for>, or make a new one from the Date
+
+          //grab the inputs id for the <label @for>, or make a new one from the Date
           //ids can start with numbers in html5, btw
-          inputId = ($this.attr('id') !== '') ? $this.attr('id') : + new Date(),
+          inputId = (this.id) ? this.id : + new Date(),
           placeholderText = $this.attr('placeholder'),
           placeholder = $('<label for='+ inputId +'>'+ placeholderText + '</label>');
         
           //stuff in some calculated values into the placeholderCSS object
           opts.placeholderCSS['width'] = inputWidth;
           opts.placeholderCSS['height'] = inputHeight;
-          // adjust position of placeholder to accomodate opera's super ugly 'email' and 'url' graphics
-          opts.placeholderCSS.left = (isOpera() && this.getAttribute('type')==='email' || isOpera() && this.getAttribute('type')==='url') ? '11%' : o_left;
+
+          // adjust position of placeholder 
+          opts.placeholderCSS.left = (isOldOpera() && (this.type == 'email' || this.type == 'url')) ?
+            '11%' : o_left;
           placeholder.css(opts.placeholderCSS);
     
       //place the placeholder if the input is empty
@@ -80,7 +84,7 @@
       'color':'#bababa', 
       'position': 'absolute', 
       'left':'5px',
-      'top':'20%', //basically for Opera, top:0 works for everyone else :/
+      'top':'3px', 
       'overflow-x': 'hidden'
     }
   };
