57cd1bb1faa02ca25958d1cb09cb2ccd3735eadf
Mike <miketaylr@gmail.com>
2010-07-22 11:15:59 -0400

    Small tweaks as per Ben Almans suggestions. Thxxx

--- README.markdown
+++ README.markdown
@@ -1,9 +1,10 @@
-# HTML5 placeholder plugin, version 1.0
+# HTML5 placeholder plugin, version 1.01
 Copyright (c) 2010-The End of Time, Mike Taylor, http://miketaylr.com
 
 MIT Licensed: http://www.opensource.org/licenses/mit-license.php
 
 Enables cross-browser html5 placeholder for inputs, by first testing for a native implementation before building one.
+Special thanks to Ben Alman (http://benalman.com/) for suggesting a number of improvements.
 
 ##USAGE: 
 `$('input[placeholder]').placeholder();`
--- html5placeholder.jquery.js
+++ html5placeholder.jquery.js
@@ -1,8 +1,8 @@
-// HTML5 placeholder plugin version 1.0
+// HTML5 placeholder plugin version 1.01
 // Copyright (c) 2010-The End of Time, Mike Taylor, http://miketaylr.com
 // MIT Licensed: http://www.opensource.org/licenses/mit-license.php
 //
-// Enables cross-browser* html5 placeholder for inputs, by first testing
+// Enables cross-browser HTML5 placeholder for inputs, by first testing
 // for a native implementation before building one.
 //
 //
@@ -11,23 +11,22 @@
 
 // <input type="text" placeholder="username">
 (function($){
+  //feature detection
+  var hasPlaceholder = 'placeholder' in document.createElement('input');
   
+  //sniffy sniff sniff -- just to give extra left padding for the older
+  //graphics for type=email and type=url
+  var isOldOpera = $.browser.opera && $.browser.version < 10.5;
+
   $.fn.placeholder = function(options) {
     //merge in passed in options, if any
-    var opts = $.extend($.fn.placeholder.defaults, options),
+    var options = $.extend({}, $.fn.placeholder.defaults, options),
     //cache the original 'left' value, for use by Opera later
-    o_left = opts.placeholderCSS.left;
-  
-    //sniffy sniff sniff -- just to give extra left padding for the older
-    //graphics for type=email and type=url
-    var isOldOpera = function() {
-      return $.browser.opera && $.browser.version < 10.5;
-    };
+    o_left = options.placeholderCSS.left;
   
     //first test for native placeholder support before continuing
     //feature detection inspired by ye olde jquery 1.4 hawtness, with paul irish
-    return ('placeholder' in document.createElement('input')) ? 
-      this : this.each(function() {
+    return (hasPlaceholder) ? this : this.each(function() {
     
       //local vars
       var $this = $(this),
@@ -36,23 +35,22 @@
           inputHeight = $this.height(),
 
           //grab the inputs id for the <label @for>, or make a new one from the Date
-          //ids can start with numbers in html5, btw
-          inputId = (this.id) ? this.id : + new Date(),
+          inputId = (this.id) ? this.id : 'placeholder' + (+new Date()),
           placeholderText = $this.attr('placeholder'),
           placeholder = $('<label for='+ inputId +'>'+ placeholderText + '</label>');
         
           //stuff in some calculated values into the placeholderCSS object
-          opts.placeholderCSS['width'] = inputWidth;
-          opts.placeholderCSS['height'] = inputHeight;
+          options.placeholderCSS['width'] = inputWidth;
+          options.placeholderCSS['height'] = inputHeight;
 
           // adjust position of placeholder 
-          opts.placeholderCSS.left = (isOldOpera() && (this.type == 'email' || this.type == 'url')) ?
+          options.placeholderCSS.left = (isOldOpera && (this.type == 'email' || this.type == 'url')) ?
             '11%' : o_left;
-          placeholder.css(opts.placeholderCSS);
+          placeholder.css(options.placeholderCSS);
     
       //place the placeholder if the input is empty
       if (!inputVal){
-        $this.wrap(opts.inputWrapper);
+        $this.wrap(options.inputWrapper);
         $this.attr('id', inputId).after(placeholder);
       };
     
